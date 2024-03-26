99b6fbf28ebd6089c74d6a7dd457906da08c17ae
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-03-26 19:20:03 -0300

    [fix] date string on log file

--- deploy.sh
+++ deploy.sh
@@ -22,52 +22,52 @@ if [ ! -d "${LOG_FILE}" ]; then
     touch ${LOG_FILE}
 fi
 
-echo "[${date}] Starting script" >> $LOG_FILE
+echo "[$(date)] Starting script" >> $LOG_FILE
 # get the branch and the path to the repository
 branch=$1
 path=$2
 
 # check if the repository exists
 if [ ! -d "$path" ]; then
-    echo "[${date}] The repository does not exist" >> $LOG_FILE
+    echo "[$(date)] The repository does not exist" >> $LOG_FILE
     exit 1
 fi
 
 # check if the branch exists
 if ! git -C $path show-ref --verify --quiet refs/heads/$branch; then
-    echo "[${date}] The branch does not exist" >> $LOG_FILE
+    echo "[$(date)] The branch does not exist" >> $LOG_FILE
     exit 1
 fi
 
 # check if the repository has any changes in the branch
 if [ -n "$(git -C $path status --porcelain)" ]; then
-    echo "[${date}] The repository has changes" >> $LOG_FILE
+    echo "[$(date)] The repository has changes" >> $LOG_FILE
     # pull the changes
-    echo "[${date}] Pulling the changes" >> $LOG_FILE
+    echo "[$(date)] Pulling the changes" >> $LOG_FILE
     git -C $path pull origin $branch >> $LOG_FILE
     # deploy the changes
-    echo "[${date}] Deploying the changes" >> $LOG_FILE
+    echo "[$(date)] Deploying the changes" >> $LOG_FILE
 
     # remove the symbolic link if it exists
     if [ -d $DINF_WEBSITE_DIR_SYMBOLIC ]; then
-        echo "[${date}] Removing symbolic link" >> $LOG_FILE
+        echo "[$(date)] Removing symbolic link" >> $LOG_FILE
         rm -rf $DINF_WEBSITE_DIR_SYMBOLIC
     fi
 
     # create the symbolic link
-    echo "[${date}] Creating symbolic link" >> $LOG_FILE
+    echo "[$(date)] Creating symbolic link" >> $LOG_FILE
     ln -vs $DINF_WEBSITE_DIR $DINF_WEBSITE_DIR_SYMBOLIC
 
     # copy the files
-    echo "[${date}] Copying files" >> $LOG_FILE
+    echo "[$(date)] Copying files" >> $LOG_FILE
     rsync -avz --delete --exclude='*.git' $WEBSITE_DIR $DINF_WEBSITE_DIR_SYMBOLIC
 
-    echo "[${date}] Changing permissions" >> $LOG_FILE
+    echo "[$(date)] Changing permissions" >> $LOG_FILE
     chmod -R 755 $DINF_WEBSITE_DIR_SYMBOLIC
 
     echo "Done"
 else
-    echo "[${date}] The repository does not have any changes" >> $LOG_FILE
+    echo "[$(date)] The repository does not have any changes" >> $LOG_FILE
 fi
 
-echo "[${date}] Script finished" >> $LOG_FILE
+echo "[$(date)] Script finished" >> $LOG_FILE
