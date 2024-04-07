b682484a129e8e6e6cb80ad91c68d977ccf0f3ce
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-04-07 20:56:34 -0300

    [add] Person, Phone, Borrowing and Book models

--- ruby_docker/bmuller_bd/config/application.rb
+++ ruby_docker/bmuller_bd/config/application.rb
@@ -18,5 +18,6 @@ module BmullerBd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths += %W(#{config.root}/lib/validators
   end
 end
