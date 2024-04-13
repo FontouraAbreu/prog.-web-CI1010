583e19ca669e608b7b9751fdf5df8d406bacbb21
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-04-13 14:33:03 -0300

    [add] county code

--- ruby_docker/t2/criaEstados.rb
+++ ruby_docker/t2/criaEstados.rb
@@ -6,4 +6,5 @@ ActiveRecord::Base.establish_connection:adapter => 'sqlite3', :database => 'Tabe
 ActiveRecord::Base.connection.create_table:estados do |t|
     t.string :sigla, limit: 2
     t.string :nome
+    t.string :codigo_uf, limit: 2
 end
