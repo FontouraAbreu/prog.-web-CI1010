bca26dd2227ed817e262402b3c83a3762bbb03f3
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-04-13 14:32:50 -0300

    [add] create and populate cities

--- ruby_docker/t2/criabd.sh
+++ ruby_docker/t2/criabd.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+SQLITE_FILE=./Tabelas.sqlite3
+
+if [ -f "$SQLITE_FILE" ]; then
+    rm $SQLITE_FILE
+fi
+
+
 ruby criaEstados.rb
+ruby criaMunicipios.rb
 ruby criaPessoas.rb
 ruby criaDocumentos.rb
 
 ruby populaEstados.rb
 ruby populaPessoas.rb
+ruby populaMunicipios.rb
