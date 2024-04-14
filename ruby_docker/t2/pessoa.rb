c8041d4b2968ed301521144c58bab89af9034a71
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-04-14 00:05:33 -0300

    [todo] fix n:n sports relation

--- ruby_docker/t2/pessoa.rb
+++ ruby_docker/t2/pessoa.rb
@@ -5,4 +5,5 @@ ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database => "Tab
 class Pessoa < ActiveRecord::Base;
     belongs_to :estado
     has_one :documento
+    has_and_belongs_to_many :esportes, ->uniq
 end
