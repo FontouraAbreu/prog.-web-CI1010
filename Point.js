0b8f866558950ee1ae90711326e0e6dada48d93c
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-04-27 13:19:33 -0300

    [add] middle line red point

--- Point.js
+++ Point.js
@@ -24,12 +24,19 @@ class Point extends Coordinate {
     }
     // draw a line between this point and the specified point
     drawToPoint(point) {
-        // draw a red dot with a smaller black dot in the center of the point
+        // draw a black dot in the center of the point
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 2, 0, 2 * Math.PI); // black dot
+        ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI); // black dot
         ctx.fillStyle = "black";
         ctx.fill();
 
+        // draw a red dot at the middle of the line
+        ctx.beginPath();
+        ctx.arc((this.x + point.x) / 2, (this.y + point.y) / 2, 3, 0, 2 * Math.PI); // red dot
+        ctx.fillStyle = "red";
+        ctx.fill();
+
+        // draw the line
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(point.x, point.y);
 
