0a8917a1225ccb05b9a03ca4ae3d99076ca11241
Vinícius Fontoura <vfa20@inf.ufpr.br>
2025-03-27 17:05:42 -0300

    [change] not taking in account git repo vars

--- deploy.sh
+++ deploy.sh
@@ -1,73 +1,48 @@
 #!/bin/bash
 # a script that checks if a repository has any changes in a specific branch and if so, pulls and deploys the changes
-# Usage: ./deploy.sh <branch> <path_to_repository>
-# Example: ./deploy.sh master /var/www/html/my-repo
-USER_GROUP=$(id -gn)
-LOG_FILE="/nobackup/${USER_GROUP}/${USER}/deploy.log"
+# Usage: ./deploy.sh
 
-WEBSITE_DIR="/home/${USER_GROUP}/${USER}/desktop/prog.-web-CI1010/"
+#loading environment variables
+export $(grep -v '^#' .env | xargs)
+
+USER_GROUP=$(id -gn)
+LOG_FILE="/nobackup/${USER_GROUP}/${USER}/${LOG_FILE_NAME}"
+
+WEBSITE_DIR="/home/${USER_GROUP}/${USER}/${WEBSITE_DIR_PATH}"
 DINF_WEBSITE_DIR="/home/html/inf/${USER}/"
 
-# BE CAREFUL WITH THE SYMBOLIC LINK AS IT WILL BE REMOVED AND CREATED AGAIN
+# BE CAREFUL WITH THE SYMBOLIC LINK AS IT WILL BE REMOVED AND CREATED AGAIN. DONT 
 DINF_WEBSITE_DIR_SYMBOLIC="/home/${USER_GROUP}/${USER}/public_html" # BE CAREFUL WITH THE SYMBOLIC LINK AS IT WILL BE REMOVED AND CREATED AGAIN
 # BE CAREFUL WITH THE SYMBOLIC LINK AS IT WILL BE REMOVED AND CREATED AGAIN
 
 
-# check if the number of arguments is correct
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./deploy.sh <branch> <path_to_repository>"
-    exit 1
-fi
 if [ ! -d "${LOG_FILE}" ]; then
     touch ${LOG_FILE}
 fi
 
 echo "[$(date)] Starting script" >> $LOG_FILE
-# get the branch and the path to the repository
-branch=$1
-path=$2
 
-# check if the repository exists
-if [ ! -d "$path" ]; then
-    echo "[$(date)] The repository does not exist" >> $LOG_FILE
-    exit 1
+# deploy the changes
+echo "[$(date)] Deploying the changes" >> $LOG_FILE
+
+# remove the symbolic link if it exists
+if [ -d $DINF_WEBSITE_DIR_SYMBOLIC ]; then
+    echo "[$(date)] Removing existing symbolic link: $DINF_WEBSITE_DIR_SYMBOLIC" >> $LOG_FILE
+    rm -rf $DINF_WEBSITE_DIR_SYMBOLIC
 fi
 
-# check if the branch exists
-if ! git -C $path show-ref --verify --quiet refs/heads/$branch; then
-    echo "[$(date)] The branch does not exist" >> $LOG_FILE
-    exit 1
-fi
+# create the symbolic link
+echo "[$(date)] Creating symbolic link" >> $LOG_FILE
+ln -vs $DINF_WEBSITE_DIR $DINF_WEBSITE_DIR_SYMBOLIC
 
-# check if the repository has any changes in the branch
-if [ -n "$(git -C $path status --porcelain)" ]; then
-    echo "[$(date)] The repository has changes" >> $LOG_FILE
-    # pull the changes
-    echo "[$(date)] Pulling the changes" >> $LOG_FILE
-    git -C $path pull origin $branch >> $LOG_FILE
-    # deploy the changes
-    echo "[$(date)] Deploying the changes" >> $LOG_FILE
+# copy the files
+echo "[$(date)] Copying files" >> $LOG_FILE
+echo "[$(date)] Excluding files: ${EXCLUDE_LIST}" >> $LOG_FILE
+rsync -avz --delete --exclude={'*.git','*.env',} --exclude=${EXCLUDE_LIST} $WEBSITE_DIR/* $DINF_WEBSITE_DIR_SYMBOLIC
 
-    # remove the symbolic link if it exists
-    if [ -d $DINF_WEBSITE_DIR_SYMBOLIC ]; then
-        echo "[$(date)] Removing symbolic link" >> $LOG_FILE
-        rm -rf $DINF_WEBSITE_DIR_SYMBOLIC
-    fi
+echo "[$(date)] Changing permissions" >> $LOG_FILE
+chmod -R 755 $DINF_WEBSITE_DIR_SYMBOLIC
 
-    # create the symbolic link
-    echo "[$(date)] Creating symbolic link" >> $LOG_FILE
-    ln -vs $DINF_WEBSITE_DIR $DINF_WEBSITE_DIR_SYMBOLIC
-
-    # copy the files
-    echo "[$(date)] Copying files" >> $LOG_FILE
-    rsync -avz --delete --exclude='*.git' $WEBSITE_DIR $DINF_WEBSITE_DIR_SYMBOLIC
-
-    echo "[$(date)] Changing permissions" >> $LOG_FILE
-    chmod -R 755 $DINF_WEBSITE_DIR_SYMBOLIC
-
-    echo "Done"
-else
-    echo "[$(date)] The repository does not have any changes" >> $LOG_FILE
-fi
+echo "Done"
 
 echo "[$(date)] Script finished" >> $LOG_FILE
